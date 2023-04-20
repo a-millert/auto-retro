f95c18efe731165f1ee183d01a127f60a63ef191
Albert Millert <124112698+a-millert@users.noreply.github.com>
2023-04-20 17:50:29 +0900

    AR-0: cleanup and refactor (#8)
    * update README
    
    * set default environment variables values in config
    
    * refactor service
    
    * rename domain model file for membership
    
    * update github actions - split into push & pr workflows

--- internal/application/service.go
+++ internal/application/service.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
 
 	application "github.com/a-millert/auto-retro/internal/application/graphql"
 	core "github.com/a-millert/auto-retro/internal/domain/model"
@@ -18,28 +19,53 @@ type Service interface {
 }
 
 type Application struct {
-	ctx          *context.Context
+	ctx          context.Context
 	client       *githubv4.Client
 	org          string
 	excludeTeams []string
 }
 
-func New(ctx *context.Context,
-	ghClient *githubv4.Client,
-	org string,
+func New(
+	githubToken string,
+	organization string,
 	excludeTeams string,
 ) *Application {
+	ctx := context.Background()
+
+	// Set up the GitHub GraphQL API v4 client with the HTTP OAuth2 client.
+	tokenSource := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	httpClient := oauth2.NewClient(ctx, tokenSource)
+	ghClient := githubv4.NewClient(httpClient)
+
 	return &Application{
 		ctx:          ctx,
 		client:       ghClient,
-		org:          org,
+		org:          organization,
 		excludeTeams: strings.Split(excludeTeams, ","),
 	}
 }
 
-func (a *Application) Login() (*core.User, error) {
+func (a *Application) Run() error {
+	// Sanity check: can log in.
+	_, err := a.login()
+	if err != nil {
+		return err
+	}
+	teams, err := a.teams()
+	if err != nil {
+		return err
+	}
+	for _, x := range teams {
+		fmt.Printf("Team %s with members: %+v\n", x.Name, x.Members)
+	}
+	return nil
+}
+
+func (a *Application) login() (*core.User, error) {
 	var loginResponse application.LoginResponse
-	err := a.client.Query(*a.ctx, &loginResponse, nil)
+	err := a.client.Query(a.ctx, &loginResponse, nil)
 	if err != nil || len(loginResponse.Viewer.Login) == 0 {
 		return nil, fail.New(err, "Failed to authenticate with the token").Abort()
 	}
@@ -48,10 +74,10 @@ func (a *Application) Login() (*core.User, error) {
 	return loginResponse.ToDomain(), nil
 }
 
-func (a *Application) Teams() ([]*core.Team, error) {
+func (a *Application) teams() ([]*core.Team, error) {
 	var teamsResponse application.TeamsResponse
 	vars := teamsResponse.InputVariables(a.org)
-	err := a.client.Query(*a.ctx, &teamsResponse, vars)
+	err := a.client.Query(a.ctx, &teamsResponse, vars)
 	if err != nil {
 		return nil, fail.New(err, "Failed to get the teams").Abort()
 	} else if len(teamsResponse.Organization.Teams.Nodes) == 0 {
